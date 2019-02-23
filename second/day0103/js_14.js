46fdadba4a58e88922fc1e8cdc0a043e7ae432b9
lzj <1602176692@qq.com>
2019-02-23 17:52:53 +0800

    16个js知识总结

---
+++ second/day0103/js_14.js
@@ -0,0 +1,29 @@
+1,在head/body中使用script标签编写JS语句
+在外部JS文件中编写js语句,通过script标签结合src属性引入
+一定要先引入外部JS在使用js文件中的内容
+2,alert / console.log / document.write 
+3,变量  let 块级作用域 var 全局作用域  常量const 
+4,基本数据类型 number string boolean undefined null 复合类型 数组 对象
+5,typeof 
+6,运算:算术运算符(+ - * / % ^ ) 逻辑运算符(&& || !) 比较运算符 (!=  ==  === > < ),
+7,流程控制
+分支(if(){} else if(){} else{} )
+switch(){case "":  ... break; },
+循环
+for(let i=0;i<10;i++){}
+while(){}
+do{}while()
+for(let index in array){} 
+8,continue 结束当前循环进入下一个循环  break 终止循环
+9,函数 :内置函数 isNaN() isFinite() parseInt()/parseFloat(),
+10,num.toString()  toString(8)
+11,function funname( ){  }
+12,内置对象 
+Math   PI floor ceil round pow abs max min random 
+String length indexof lastindexof charAt substr substring replace toLowercase toUppercase split 
+Date  getfullyear getmonth getdate getday gethours getminutes getseconds gettime 
+Array length push pop shift unshift indexof reverse sort splice 
+13 ,JSON  序列化 将对象转字符串 stringify   字符串转对象 parse
+14, setTimeout(fn,2000)  setInterval(fn,2000)     clearTimeout()  clearInterval()
+15, 匿名函数 function(){} 
+16, 闭包
